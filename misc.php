8f457062e25f0988348e0c944cfc15a2be6bc39d
sunkang <13127955213@qq.com>
2023-11-16 14:32:59 +0800

    new version

--- misc.php
+++ misc.php
@@ -1,13 +1,4 @@
-<?php
-
-/*
-    Copyright (c) 2021-2031, All rights reserved.
-    This is NOT a freeware
-    LICENSE: https://github.com/thefunpower/core/blob/main/LICENSE.md 
-    Connect Email: sunkangchina@163.com 
-    Code Vesion: v1.0.x
-*/
-
+<?php 
 global $misc_config,$config;
 $misc_ini = PATH.'boot/misc.ini.php';
 if(file_exists($misc_ini)){
